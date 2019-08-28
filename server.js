19b354cc0ace2ed9d0c8540a2d953b382d442ec8
Sashika Nawarathne <cazci269@gmail.com>
2019-08-28 21:46:21 +0530

    Use express app in server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(port);
