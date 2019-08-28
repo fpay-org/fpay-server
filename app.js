3a28bbdf40d530f4389db49dfafed49cc91bc706
Sashika Nawarathne <cazci269@gmail.com>
2019-08-28 18:02:32 +0530

    Handle unknown routes

--- app.js
+++ app.js
@@ -7,5 +7,20 @@ const fineRoutes = require("./api/routes/fines");
 /* Use imported routes in the app */
 app.use("/fines", fineRoutes);
 
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
 /* Export module */
 module.exports = app;
