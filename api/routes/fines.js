d6e6d932adb04985a0fa5235a400b62ecc4a6192
Sashika Nawarathne <cazci269@gmail.com>
2019-08-28 17:58:10 +0530

    Add comments

--- api/routes/fines.js
+++ api/routes/fines.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+/* /fines - return all fines */
 router.get("/", (req, res, next) => {
   res.status(200).json({
-    message: "Handling GET "
+    message: "Handling GET"
   });
 });
+
+/* /fines/issue - issue new fine */
+
+/* /fines/:id - return single fine */
+
+/* fines/update/:id - update issued fine */
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
+/* Import routes */
 const fineRoutes = require("./api/routes/fines");
 
+/* Use imported routes in the app */
 app.use("/fines", fineRoutes);
 
+/* Export module */
 module.exports = app;
