d0ec75c989d374543a291d9fcb03ec62285cfd58
Sashika Nawarathne <cazci269@gmail.com>
2019-08-30 23:03:24 +0530

    Install body parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 /* Import routes */
 const fineRoutes = require("./api/routes/fines");
@@ -7,12 +8,18 @@ const fineRoutes = require("./api/routes/fines");
 /* Use imported routes in the app */
 app.use("/fines", fineRoutes);
 
+/* Middleware */
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/* Handle invalid routes */
 app.use((req, res, next) => {
   const error = new Error("Not Found");
   error.status = 404;
   next(error);
 });
 
+/* Catch invalid router errors */
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({
