eae176cd365a41300e8080f7d16cd9d0c1e5bc4f
Sashika Nawarathne <cazci269@gmail.com>
2019-09-22 21:32:09 +0530

    Prevent duplicate entry

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,35 +6,45 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 router.post("/register", (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
-      return res.status(500).json({
-        error: err
-      });
-    } else {
-      const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        username: req.body.username,
-        email: req.body.email,
-        password: hash
-      });
-
-      user
-        .save()
-        .then(result => {
-          console.log("User created\nObject: " + result);
-          res.status(201).json({
-            message: "User created"
-          });
-        })
-        .catch(error => {
-          console.log(error);
-          res.status(500).json({
-            error: error
-          });
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "Mail already exists"
         });
-    }
-  });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err
+            });
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              username: req.body.username,
+              email: req.body.email,
+              password: hash
+            });
+
+            user
+              .save()
+              .then(result => {
+                console.log("User created\nObject: " + result);
+                res.status(201).json({
+                  message: "User created"
+                });
+              })
+              .catch(error => {
+                console.log(error);
+                res.status(500).json({
+                  error: error
+                });
+              });
+          }
+        });
+      }
+    });
 });
 
 module.exports = router;
