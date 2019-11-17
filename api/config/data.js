1e95c94296e00ff92d3d3d07a125ff5b505dcbc5
Sashika Nawarathne <cazci269@gmail.com>
2019-11-17 17:31:45 +0530

    Reveal config data

---
+++ api/config/data.js
@@ -0,0 +1,7 @@
+const data = {
+  MONGO_DB:
+    "mongodb+srv://server:fpaydb@cluster0-wedr9.gcp.mongodb.net/test?retryWrites=true&w=majority",
+  JWT_SECRET: "fpaysecret"
+};
+
+module.exports = data;
