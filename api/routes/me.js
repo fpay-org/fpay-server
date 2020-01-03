5d515327435433b00fd6245e1d5c688cfd35e5bb
Sashika Nawarathne <cazci269@gmail.com>
2020-01-04 03:09:45 +0530

    Me route handler

--- api/routes/me.js
+++ api/routes/me.js
@@ -1,43 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
 
-const data = require("../config/data");
+const handler = require("../handlers/me");
 
-const response = require("../utils/response");
-const Logger = require("../utils/logger");
-const logger = new Logger();
-
-router.get("/:token", (req, res, next) => {
-  const token = jwt.verify(req.params.token, data.JWT_SECRET);
-
-  if (token.role == 0) {
-    // Handle driver
-    const Driver = require("../models/driver");
-
-    Driver.findById(token.id)
-      .exec()
-      .then(driver => {
-        return response(res, 200, "Success", driver);
-      })
-      .catch(err => {
-        logger.error(err);
-        return response(res, 500, err);
-      });
-  } else {
-    const Officer = require("../models/officer");
-
-    Officer.findById(token.id)
-      .exec()
-      .then(officer => {
-        return response(res, 200, "Success", officer);
-      })
-      .catch(err => {
-        logger.error(err);
-        return response(res, 500, err);
-      });
-  }
-});
+router.get("/me", handler.get);
 
 module.exports = router;
