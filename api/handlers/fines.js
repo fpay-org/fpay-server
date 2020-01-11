70804a5fa50f540c54514b60d8478cf5d82f4a09
Sashika Nawarathne <cazci269@gmail.com>
2020-01-11 11:48:36 +0530

    Update fine route

--- api/handlers/fines.js
+++ api/handlers/fines.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const Fine = require("../models/fine");
 const Driver = require("../models/driver");
+const Vehicle = require("../models/vehicle");
 
 const response = require("../utils/response");
 const Logger = require("../utils/logger");
@@ -16,21 +17,31 @@ exports.getAll = async (req, res) => {
 };
 
 exports.create = async (req, res) => {
+  const driver_id = await Driver.findOne({ nid: req.body.driver_nid })
+    .exec()
+    .then(driver => driver._id)
+    .catch(err => response(res, null, 500, err));
+
+  const vehicle_id = await Vehicle.findOne({
+    license_number: req.body.license_number
+  })
+    .exec()
+    .then(vehicle => vehicle._id)
+    .catch(err => response(res, null, 500, err));
+
   const fine = new Fine({
     _id: new mongoose.Types.ObjectId(),
     total_value: req.body.total_value,
     currency: req.body.currency,
     penalties: req.body.penalties,
-    driver: req.body.driver,
+    driver: driver_id,
     officer: req.body.officer,
     secondary_officer: req.body.secondary_officer,
     location: req.body.location,
-    vehicle: req.body.vehicle,
+    vehicle: vehicle_id,
     image_url: req.body.image_url,
     issued_at: Date.now()
   });
-
-  logger.info("Was here");
 
   fine
     .save()
