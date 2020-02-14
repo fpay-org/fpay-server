a46c86113f8b68790c762cc1117f5481e5ed59a7
Sankha Jayalath <sankha.rc@gmail.com>
2020-02-14 15:39:14 +0530

    dashboard handler changed

--- api/handlers/dashboard.js
+++ api/handlers/dashboard.js
@@ -16,7 +16,7 @@ const logger = new Logger();
 
 // Should paginate
 exports.getAll = async (req, res) => {
-  Dashboard.find()
+  Dashboards.find()
     .exec()
     .then(posts => response(res, posts))
     .catch(err => response(res, null, 500, err));
