ea3d5cfbadb27d5fc9077abe83c3fbd352c4a1c2
Sashika Nawarathne <cazci269@gmail.com>
2020-02-16 08:33:41 +0530

    Minor update

--- api/routes/driver.js
+++ api/routes/driver.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const handler = require("../handlers/driver");
 
+router.get('/', handler.getAll);
+router.get("/:nid", handler.getOne)
 router.post("/:nid", handler.update);
 
 module.exports = router;
