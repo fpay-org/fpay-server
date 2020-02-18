0835937d03a08232e733e23170b246b5010025f0
Sashika Nawarathne <cazci269@gmail.com>
2020-02-18 07:51:23 +0530

    Update forget password

--- api/routes/officer.js
+++ api/routes/officer.js
@@ -14,7 +14,7 @@ const handler = require("../handlers/officer");
 router.get("/", handler.getAll);
 router.get("/:officer_id", handler.getOne);
 router.post("/:officer_id", handler.update);
-router.post("/pass/:officer_id", handler.passUpdate);
+router.post("/:officer_id/password-update", handler.passUpdate);
 router.post(
   "/:officer_id/avatar",
   upload.single("officer_image"),
